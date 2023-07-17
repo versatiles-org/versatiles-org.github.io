3d41c107368dd80cfa44e3170ff5c41bbe8bf918
Michael Kreil <github@michael-kreil.de>
2023-07-18 00:37:47 +0200

    add Vec abstraction

--- chart.js
+++ chart.js
@@ -54,7 +54,7 @@ export class Chart {
 				fill: color + '5',
 			});
 			rect[0] += this.boxHeight / 3;
-			group.drawText(rect, text, { fill: color, fontFamily, fontSize: this.boxHeight / 3 });
+			group.drawText(rect, text, { fill: color, fontFamily, fontSize: 12 });
 		})
 		this.y0 += this.boxHeight;
 		this.flowYMax = this.y0;
@@ -111,6 +111,7 @@ export class Chart {
 		let x = this.colStart + (col + 0.5) * this.colWidth - this.boxWidth / 2;
 		let box = this.#drawContainer(this.dependencyGroup, [x, this.depColY[col]], type, name);
 		this.depColY[col] += this.boxHeight * 1.5;
+		this.y0 = Math.max(this.y0, this.depColY[col]);
 
 		if (ref) {
 			let path = this.#getPath(ref, box, options);
@@ -124,7 +125,29 @@ export class Chart {
 		options.shortenEnd = 5;
 		let path = this.#getPath(dep0, dep1, options);
 		this.dependencyGroup.drawPath(path.d, { fill: 'none', stroke: dep0.color, strokeWidth: 2 })
-		this.dependencyGroup.drawArrowHead(path.point1, path.dir1.map(v => 10 * v), { fill: dep0.color })
+		this.dependencyGroup.drawArrowHead(
+			path.point1.array(),
+			path.dir1.scale(10).array(),
+			{ fill: dep0.color }
+		)
+	}
+
+	addRepo(name, col, refs = [], options = {}) {
+		this.repoGroup ??= this.canvas.appendGroup();
+		this.repoColY ??= [this.y0, this.y0, this.y0, this.y0];
+
+		if (options.dy) this.repoColY[col] += options.dy;
+		let x = this.colStart + (col + 0.5) * this.colWidth - this.boxWidth / 2;
+		let box = this.#drawContainer(this.repoGroup, [x, this.repoColY[col]], 'repo', name);
+		this.repoColY[col] += this.boxHeight * 1.5;
+		this.y0 = Math.max(this.y0, this.depColY[col]);
+
+		refs.forEach(ref => {
+			let path = this.#getPath(ref, box, options);
+			this.repoGroup.drawPath(path.d, { fill: 'none', stroke: box.color + 'A', strokeWidth: 1 })
+		})
+
+		return box;
 	}
 
 	#getType(type) {
@@ -133,6 +156,7 @@ export class Chart {
 			case 'rust': return ['#0AF', 'Rust Package'];
 			case 'npm': return ['#0FF', 'NPM Package'];
 			case 'file': return ['#0FA', 'File'];
+			case 'repo': return ['#AAA', 'GitHub Repository'];
 			default: throw Error(type);
 		}
 	}
@@ -143,57 +167,41 @@ export class Chart {
 		opt.offset ??= 20;
 		opt.radius ??= 0;
 
-		let size0 = [box0.rect[2] / 2, box0.rect[3] / 2];
-		let size1 = [box1.rect[2] / 2, box1.rect[3] / 2];
-		let point0 = [box0.rect[0] + size0[0], box0.rect[1] + size0[1]];
-		let point1 = [box1.rect[0] + size1[0], box1.rect[1] + size1[1]];
-		let dx = point0[0] - point1[0];
-		let dy = point0[1] - point1[1];
-		let dir0;
-		if (Math.abs(dx) > Math.abs(dy)) {
-			dir0 = (dx < 0) ? [1, 0] : [-1, 0];
-		} else {
-			dir0 = (dy < 0) ? [0, 1] : [0, -1];
-		}
-		let dir1 = dir0.map(v => -v);
+		let size0 = new Vec(box0.rect[2], box0.rect[3]).scale(0.5);
+		let size1 = new Vec(box1.rect[2], box1.rect[3]).scale(0.5);
 
-		if (opt.startDir) dir0 = dirLookup(opt.startDir);
-		if (opt.endDir) dir1 = dirLookup(opt.endDir);
+		let center0 = new Vec(box0.rect[0], box0.rect[1]).add(size0);
+		let center1 = new Vec(box1.rect[0], box1.rect[1]).add(size1);
 
-		let con0 = p(i => point0[i] + (size0[i] - opt.radius + opt.offset) * dir0[i]);
-		let con1 = p(i => point1[i] + (size1[i] - opt.radius + opt.offset) * dir1[i]);
+		let dir0 = center0.getDirection(center1).orthogonalize().normalize();
+		let dir1 = dir0.clone().scale(-1);
 
-		return {
-			point0: p(i => point0[i] + size0[i] * dir0[i]), dir0,
-			point1: p(i => point1[i] + size1[i] * dir1[i]), dir1,
-			d: [
-				'M',
-				p(i => point0[i] + (size0[i] + opt.shortenStart) * dir0[i]).join(','),
-				'L',
-				con0.join(','),
+		if (opt.startDir) dir0 = Vec.fromChar(opt.startDir);
+		if (opt.endDir) dir1 = Vec.fromChar(opt.endDir);
 
+		let distance0 = Math.abs(dir0.scalar(size0));
+		let distance1 = Math.abs(dir1.scalar(size1));
 
+		let contact0 = center0.clone().addScaled(dir0, distance0);
+		let contact1 = center1.clone().addScaled(dir1, distance1);
 
-				'L',
-				con1.join(','),
-				'L',
-				p(i => point1[i] + (size1[i] + opt.shortenEnd) * dir1[i]).join(',')
-			].join('')
+		let start0 = center0.clone().addScaled(dir0, distance0 + (opt.shortenStart || 0));
+		let start1 = center1.clone().addScaled(dir1, distance1 + (opt.shortenEnd || 0));
+
+		let result = {
+			point0: contact0, dir0,
+			point1: contact1, dir1
 		};
+		//console.log({ center0, dir0, size0, contact0 });
 
-		function p(cb) {
-			return [0, 1].map(i => cb(i));
+		if (contact0.getDirection(contact1).isOrthogonal()) {
+			return { ...result, d: 'M' + start0.str() + 'L' + start1.str() }
 		}
 
-		function dirLookup(text) {
-			switch (text.toUpperCase()) {
-				case 'N': return [0, -1];
-				case 'S': return [0, 1];
-				case 'W': return [-1, 0];
-				case 'E': return [1, 0];
-				default: throw Error();
-			}
-		}
+		let connnection0 = start0.clone().addScaled(dir0, opt.offset);
+		let connnection1 = start1.clone().addScaled(dir1, opt.offset);
+
+		return { ...result, d: 'M' + contact0.str() + 'L' + contact1.str() }
 	}
 
 	#drawContainer(canvas, pos, type, name) {
@@ -211,9 +219,106 @@ export class Chart {
 
 		canvas.drawText(
 			[pos[0], pos[1] + 10, this.boxWidth, this.boxHeight - 10], name,
-			{ fill: color, fontFamily, fontSize: this.boxHeight / 3 }
+			{ fill: color, fontFamily, fontSize: 12 }
 		);
 
 		return { rect, color };
+	}
+}
+/*
+class Direction {
+	constructor(input) {
+		if (Array.isArray(input)) {
+			this.x = input[0];
+			this.y = input[1];
+			return
+		} else if (typeof input === 'string') {
+			// compass
+			throw Error();
+
+			function dirLookup(text) {
+				switch (text.toUpperCase()) {
+					case 'N': return [0, -1];
+					case 'S': return [0, 1];
+					case 'W': return [-1, 0];
+					case 'E': return [1, 0];
+					default: throw Error();
+				}
+			}
+		}
+		throw Error();
+	}
+	add(point) {
+		this.x += point.x;
+		this.y += point.y;
+		return this;
+	}
+}*/
+
+class Vec {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+	clone() {
+		return new Vec(this.x, this.y);
+	}
+	add(vec) {
+		this.x += vec.x;
+		this.y += vec.y;
+		return this;
+	}
+	scalar(vec) {
+		return this.x * vec.x + this.y * vec.y;
+	}
+	scale(v) {
+		this.x *= v;
+		this.y *= v;
+		return this;
+	}
+	multiply(vec) {
+		this.x *= vec.x;
+		this.y *= vec.y;
+		return this;
+	}
+	normalize() {
+		let r = Math.sqrt(this.x * this.x + this.y * this.y)
+		this.x /= r;
+		this.y /= r;
+		return this;
+	}
+	getDirection(vec) {
+		return new Vec(vec.x - this.x, vec.y - this.y).normalize();
+	}
+	addScaled(vec, scale) {
+		this.x += vec.x * scale;
+		this.y += vec.y * scale;
+		return this;
+	}
+	orthogonalize() {
+		if (Math.abs(this.x) < Math.abs(this.y)) {
+			this.x = 0;
+		} else {
+			this.y = 0;
+		}
+		return this;
+	}
+	isOrthogonal() {
+		return (Math.abs(this.x) < 1e-10) || (Math.abs(this.y) < 1e-10)
+	}
+	str() {
+		return this.x + ',' + this.y
+	}
+	array() {
+		return [this.x, this.y]
+	}
+	static fromChar(char) {
+		switch (char.toUpperCase()) {
+			case 'N': return new Vec(0, -1);
+			case 'S': return new Vec(0, 1);
+			case 'W': return new Vec(-1, 0);
+			case 'E': return new Vec(1, 0);
+			default: throw Error();
+		}
 	}
 }
