9fe9d606a84a770685aeeda2e216bbc518537cea
Michael Kreil <github@michael-kreil.de>
2023-07-23 00:22:43 +0200

    add watch mode

--- src/cms.js
+++ src/cms.js
@@ -1,13 +1,14 @@
 
 import Handlebars from 'handlebars';
 import { resolve } from 'node:path';
-import { existsSync, mkdirSync, rmSync } from 'node:fs';
+import { existsSync, mkdirSync, rmSync, watch } from 'node:fs';
 import { fileURLToPath } from 'node:url';
 
 const projectPath = resolve(fileURLToPath(import.meta.url), '../../');
 
 const config = {
 	src: {
+		root: resolve(projectPath, 'docs'),
 		pages: resolve(projectPath, 'docs/pages'),
 		assets: resolve(projectPath, 'docs/assets'),
 		graphics: resolve(projectPath, 'docs/graphics'),
@@ -20,18 +21,19 @@ const config = {
 	}
 }
 
-await build()
+if (existsSync(config.dst.root)) rmSync(config.dst.root, { recursive: true });
+mkdirSync(config.dst.root);
 
-async function build() {
-	let handlebars = Handlebars.create();
+await build();
 
-	if (existsSync(config.dst.root)) rmSync(config.dst.root, { recursive: true });
-	mkdirSync(config.dst.root);
-
-	run('build', config, handlebars);
+if (process.argv[2] === 'watch') {
+	watch(config.src.root, { recursive: true }, async () => { await build() });
 }
 
-async function run(command, ...args) {
+async function build() {
+	let t = Date.now();
+	let handlebars = Handlebars.create();
+
 	let modules = [
 		'assets',
 		'helpers',
@@ -42,6 +44,8 @@ async function run(command, ...args) {
 	for (let module of modules) {
 		module = `./lib/${module}.js`;
 		module = await import(module);
-		await module[command](...args)
+		await module.build(config, handlebars)
 	}
+
+	console.log('rebuild:', (Date.now() - t) + 'ms')
 }
