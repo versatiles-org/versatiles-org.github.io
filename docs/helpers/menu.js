6e8167ecbe7f786e6a99afda3c4ccabeaabc31fa
Michael Kreil <github@michael-kreil.de>
2023-08-13 13:35:43 +0200

    reorganize pages

--- docs/helpers/menu.js
+++ docs/helpers/menu.js
@@ -4,9 +4,10 @@ export default function (arg) {
 	if (filename === 'index.html') filename = '';
 
 	let links = [
-		{ href: '', title: 'Introduction' },
-		{ href: 'how_to_use.html', title: 'How to use' },
-		{ href: 'how_to_help.html', title: 'Contribute' },
+		{ href: '', title: 'Demo' },
+		{ href: 'guide.html', title: 'Intro' },
+		{ href: 'overview.html', title: 'Overview' },
+		{ href: 'contribute.html', title: 'Help' },
 	];
 
 	links.forEach(l => {
