a03bb62c203412c06fe6c4f4877512fc489988ad
Michael Kreil <github@michael-kreil.de>
2025-05-29 19:46:26 +0200

    refactor: simplify build

--- src/build.ts
+++ src/build.ts
@@ -1,7 +1,7 @@
+import { resolve } from '@std/path/resolve';
 import CMS from './cms/index.ts';
 
-const SRC_PATH = new URL('../docs', import.meta.url).pathname;
-const DST_PATH = new URL('../dist', import.meta.url).pathname;
+const PATH = new URL('..', import.meta.url).pathname;
 
-const cms = new CMS(SRC_PATH, DST_PATH);
+const cms = new CMS(resolve(PATH, 'docs'), resolve(PATH, 'dist'));
 await cms.build();
