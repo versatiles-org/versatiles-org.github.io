bb0df4cd970114277881e35672e616594259a6e3
Michael Kreil <github@michael-kreil.de>
2025-10-08 11:34:53 +0200

    test: fix asset directory structure

--- src/cms/index.test.ts
+++ src/cms/index.test.ts
@@ -8,11 +8,12 @@ describe('CMS builds site structure', () => {
 	// Arrange
 	const srcPath = 'test_src';
 	const dstPath = 'test_dst';
-	Deno.mkdirSync(join(srcPath, 'assets', 'style'), { recursive: true });
-	Deno.writeTextFileSync(join(srcPath, 'assets', 'style', 'main.less'), 'body{}');
-	Deno.writeTextFileSync(join(srcPath, 'assets', 'style', 'menu.less'), 'nav{}');
-	Deno.writeTextFileSync(join(srcPath, 'assets', 'style', 'hero.less'), '.hero{}');
-	Deno.writeTextFileSync(join(srcPath, 'assets', 'logo.png'), 'PNGDATA');
+	Deno.mkdirSync(join(srcPath, 'assets/style'), { recursive: true });
+	Deno.writeTextFileSync(join(srcPath, 'assets/style/main.less'), 'body{}');
+	Deno.writeTextFileSync(join(srcPath, 'assets/style/menu.less'), 'nav{}');
+	Deno.writeTextFileSync(join(srcPath, 'assets/style/hero.less'), '.hero{}');
+	Deno.writeTextFileSync(join(srcPath, 'assets/style/ignore.png'), '');
+	Deno.writeTextFileSync(join(srcPath, 'assets/logo.png'), 'PNGDATA');
 	Deno.writeTextFileSync(
 		join(srcPath, 'test.md'),
 		[
